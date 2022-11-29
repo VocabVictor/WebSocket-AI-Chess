82ca34d3f163dcc960c6c9ac49b7ec90780d0f4a
吴忠恒 <2402252516@qq.com>
2022-11-29 18:40:29 +0800

    init

---
+++ src/main/java/com/example/wevsocketaichess/WevSocketAiChessApplication.java
@@ -0,0 +1,13 @@
+package com.example.wevsocketaichess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WevSocketAiChessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WevSocketAiChessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wevsocketaichess/WevSocketAiChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wevsocketaichess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WevSocketAiChessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
